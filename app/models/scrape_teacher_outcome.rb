845a657ab169cc54b986368829ebf0f9dec7d997
shayashi683 <shayashi683@gmail.com>
2020-08-20 21:49:08 +0900

    save scrape_teacher_outcome to db

--- app/models/scrape_teacher_outcome.rb
+++ app/models/scrape_teacher_outcome.rb
@@ -24,31 +24,42 @@ class ScrapeTeacherOutcome < ApplicationRecord
         elements.each_with_index do |element, i|
             # もしactiveフラグがオンだったらリンクを取得する(activeクラスを持った次のliは次へのリンクなはず)
             if active_flag == 1
-            next_link = element.at("a")["href"]
-            break
+                next_link = element.at("a")["href"]
+                break
             end
             # liにactiveクラスがついていたらフラグを立てる
             if element.first.present? && element.first[1] == "active"
-            active_flag = 1
+                active_flag = 1
             # 前回のループと同じリンクだった場合、while文を抜けるためにactive_flagをオフにする
             if i != 0 && pre_next_link == next_link
                 active_flag = 0
             else
+
+                next_links = domain + next_link
+                p next_links
                 # ここに対象のページでやりたい処理を書いていく
-                elements = current_page.search('.rm-cv-list-content a')
-                    elements.each do |ele|
-                        ele.inner_text
-                    end
-                sum = sum + elements.count
+                    # next_links.each do |link|
+                    #     get_book(link)
+                    # end
+                # sum = sum + elements.count
             end
             pre_next_link = next_link
             end
         end
         default_page = default_page + page_count
         next_url = "/read0170729/published_papers?limit=20&offset=#{default_page}"
+        # next_links = domain + next_link
         page_num = page_num + 1
             end
-        outcome = array.push(sum)
-        outcome.save
+        # p data1 = array.push(sum)
+        # p next_links
         end
+
+        # def self.get_book(link)
+        #     agent = Mechanize.new
+        #     page = agent.get(link)
+
+        #     book.data1 = page.search('.rm-cv-list-content a').inner_text
+        #     book.save
+        # end
 end
